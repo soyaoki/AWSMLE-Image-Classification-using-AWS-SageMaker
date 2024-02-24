77604cc85361afd485a8ca701f06bf6a62748d15
Soya AOKI <45820850+soyaoki@users.noreply.github.com>
2024-02-24 22:31:55 +0900

    update: Updated scripts.

---
+++ hpo.py
@@ -0,0 +1,198 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import argparse
+import json
+import logging
+import os
+import sys
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+def test(model, test_loader, criterion, device):
+    '''
+    TODO: Complete this function that can take a model and a 
+          testing data loader and will get the test accuray/loss of the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data = data.to(device)
+            target = target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    logger.info(
+        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
+
+def train(model, train_loader, criterion, optimizer, epochs, device):
+    '''
+    TODO: Complete this function that can take a model and
+          data loaders for training and will get train the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    for epoch in range(1, epochs + 1):
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader, 1):
+            data = data.to(device)
+            target = target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % 100 == 0:
+                logger.info(
+                    "Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(data),
+                        len(train_loader.dataset),
+                        100.0 * batch_idx / len(train_loader),
+                        loss.item(),
+                    )
+                )
+    return model
+    
+def net():
+    '''
+    TODO: Complete this function that initializes your model
+          Remember to use a pretrained model
+    '''
+    model = models.resnet18(pretrained = True)
+
+    for param in model.parameters():
+        param.requires_grad = False   
+
+    num_features = model.fc.in_features
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 133))
+    return model
+
+def create_data_loaders(data_path, batch_size, shuffle = False, transform = None):
+    '''
+    This is an optional function that you may or may not need to implement
+    depending on whether you need to use data loaders or not
+    '''
+    dataset = torchvision.datasets.ImageFolder(root = data_path, transform = transform)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size = batch_size, shuffle = shuffle)
+    
+    return data_loader
+
+def main(args):
+    '''
+    TODO: Initialize a model by calling the net function
+    '''
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    logger.info(f"Running on Device {device}")
+    logger.info("Creating model.")
+    model = net()
+    model = model.to(device)
+    
+    '''
+    TODO: Create your loss and optimizer
+    '''
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), args.lr)
+
+    '''
+    TODO: Create dataloaders
+    '''
+    logger.info("Creating dataloaders.")
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    train_loader = create_data_loaders(os.path.join(args.data_dir, 'train'), args.batch_size, shuffle=True, transform=train_transform)
+    
+    test_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    test_loader = create_data_loaders(os.path.join(args.data_dir, 'valid'), args.batch_size, transform=test_transform)
+    
+    '''
+    TODO: Call the train function to start training your model
+    Remember that you will need to set up a way to get training data from S3
+    '''
+    logger.info("Training the model.")
+    model = train(model, train_loader, loss_criterion, optimizer, args.epochs, device)
+    
+    '''
+    TODO: Test the model to see its accuracy
+    '''
+    logger.info("Testing the model.")
+    test(model, test_loader, loss_criterion, device)
+    
+    '''
+    TODO: Save the trained model
+    '''
+    logger.info("Saving the model.")
+    path = os.path.join(args.model_dir, "model.pth")
+    torch.save(model.state_dict(), path)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    '''
+    TODO: Specify all the hyperparameters you need to use to train your model.
+    '''
+    # Data and model checkpoints directories
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=5,
+        metavar="N",
+        help="number of epochs to train (default: 10)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.01, metavar="LR", help="learning rate (default: 0.01)"
+    )
+    # Container environment
+    parser.add_argument(
+        "--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAINING"]
+    )
+    parser.add_argument(
+        "--model-dir", type=str, default=os.environ["SM_MODEL_DIR"]
+    )
+    
+    # parser.add_argument("--data-dir", type=str, default='./dogImages')
+    # parser.add_argument("--model-dir", type=str, default='./outputs')
+    
+    # parser.add_argument("--hosts", type=list, default=json.loads(os.environ["SM_HOSTS"]))
+    # parser.add_argument("--current-host", type=str, default=os.environ["SM_CURRENT_HOST"])
+    # parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
+    
+    args = parser.parse_args()
+    print(args)
+    
+    main(args)
